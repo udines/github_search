46a302857464fe64190894d695ac10b5c1e3f8ff
FATA <fataazharuddin.qontak@outlook.com>
2019-12-21 23:33:40 +0700

    Add github search bloc

---
+++ lib/blocs/github_search_bloc.dart
@@ -0,0 +1,54 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:githubsearch/bloc/github_search_event.dart';
+import 'package:githubsearch/bloc/github_search_state.dart';
+import 'package:githubsearch/models/search_result_error.dart';
+import 'package:githubsearch/services/github_repository.dart';
+import 'package:rxdart/rxdart.dart';
+
+class GithubSearchBloc extends Bloc<GithubSearchEvent, GithubSearchState> {
+  final GithubRepository githubRepository;
+
+  GithubSearchBloc({@required this.githubRepository});
+
+  @override
+  Stream<GithubSearchState> transformEvents(
+    Stream<GithubSearchEvent> events, 
+    Stream<GithubSearchState> Function(GithubSearchEvent event) next
+  ) {
+    return super.transformEvents(
+      (events as Observable<GithubSearchEvent>).debounceTime(
+        Duration(milliseconds: 500)
+      ),
+      next
+    );
+  }
+
+  @override
+  void onTransition(Transition<GithubSearchEvent, GithubSearchState> transition) {
+    print(transition);
+  }
+
+  @override
+  Stream<GithubSearchState> mapEventToState(GithubSearchEvent event) async* {
+    if (event is TextChanged) {
+      final String searchTerm = event.text;
+      if (searchTerm.isEmpty) {
+        yield SearchStateEmpty();
+      } else {
+        yield SearchStateLoading();
+        try {
+          final results = await githubRepository.search(searchTerm);
+          yield SearchStateSuccess(results.items);
+        } catch(error) {
+          yield error is SearchResultError
+            ? SearchStateError(error.message)
+            : SearchStateError('Something went wrong');
+        }
+      }
+    }
+  }
+
+  @override
+  GithubSearchState get initialState => SearchStateEmpty();
+}
---
+++ lib/services/github_repository.dart
@@ -0,0 +1,20 @@
+import 'package:githubsearch/models/search_result.dart';
+import 'package:githubsearch/services/github_cache.dart';
+import 'package:githubsearch/services/github_client.dart';
+
+class GithubRepository {
+  final GithubClient client;
+  final GithubCache cache;
+
+  GithubRepository({this.client, this.cache});
+
+  Future<SearchResult> search(String term) async {
+    if (cache.contains(term)) {
+      return cache.get(term);
+    } else {
+      final result = await client.search(term);
+      cache.set(term, result);
+      return result;
+    }
+  }
+}
